20209bdc5578825d69e98509600e4c6db588b09e
JamesEe@MacBook <james.ee.sg@gmail.com>
2024-08-06 01:09:44 +0000

    read_toml module updatedd

--- mypackages/src/__main__.py
+++ mypackages/src/__main__.py
@@ -8,10 +8,14 @@ def main():
     print(hello1())
     print(hello2())
     print(submodule4())
-    
+
     pyproject = read_toml('pyproject.toml')
     for dep in pyproject['tool']['poetry']['dependencies']:
         print(dep)
+
+    # for dep in pyproject['tool']['poetry']['group']['dev']['dependencies']:
+    #     print(dep)
+    
 
 if __name__=="__main__":
     main()
--- mypackages/src/utils/read_toml.py
+++ mypackages/src/utils/read_toml.py
@@ -2,11 +2,4 @@ import tomllib
 
 def read_toml(filename):
     with open(filename, 'rb') as f:
-        pyproject = tomllib.load(f)
-    return pyproject
-
-    # for dep in pyproject['tool']['poetry']['group']['dev']['dependencies']:
-    #     print(dep)
-    
-    # for dep in pyproject['tool']['poetry']['dependencies']:
-    #     print(dep)
+        return tomllib.load(f)
